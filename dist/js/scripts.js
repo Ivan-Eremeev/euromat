2b4c32988c0625245530d113fb94de4e008a2597
Ivan <ivan.frontcoder@gmail.com>
2021-09-08 18:57:15 +0500

    .

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -11,63 +11,53 @@ $(document).ready(function () {
 			breakMd = 1025,
 			breakSm = 769,
 			breakXs = 500;
-	
-	// Подключение настроек плагинов. Использовать "//=" перед строкой пути
-	// libs-settings/fancybox_settings.js
-	// libs-settings/slick_settings.js
-	// libs-settings/fullpage_settings.js
-	// libs-settings/tinyscrollbar-settings.js
-	// libs-settings/tooltipster-settings.js
-	// libs-settings/yandex-map-settings.js
-	// libs-settings/google-map-settings.js
-	// mailto-ajax.js
 
 	// Запрет перехода по ссылкам с хэшем
 	$('a[href="#"]').click(function(e) {
 		e.preventDefault();
 	});
 
-	// Мобильное меню
-	function myMenu(menu) {
-		if (menu.length) {
-			menu.each(function () {
-				var $this = $(this),
-						menuBtn = $this.find('#menu-btn'),
-						over = $this.find('#menu-over'),
-						close = $this.find('#menu-close'),
-						body = $('body'),
-						scrollbarWidth;
-				menuBtn.on('click', toggleOpenMenu);
-				over.on('click', menuClose);
-				close.on('click', menuClose);
-				function menuOpen() { // Открывание меню
-					body.addClass('lock').css('padding-right', scrollbarWidth);
-					$this.addClass('open');
-					menuBtn.addClass('is-active');
-				}
-				function menuClose() { // Закрывание меню
-					body.removeClass('lock').css('padding-right', 0);
-					$this.removeClass('open');
-					menuBtn.removeClass('is-active');
-				}
-				function scrollbarWidthCalc() { // Вычисление ширины скролла
-					var documentWidth = parseInt(document.documentElement.clientWidth),
-							windowsWidth = parseInt(window.innerWidth);
-							scrollbarWidth = windowsWidth - documentWidth;
-				}
-				function toggleOpenMenu() { // Открывание/закрывание меню
-					if ($this.hasClass('open')) {
-						menuClose();
-					}else {
-						menuOpen();
-					}
-				}
-				scrollbarWidthCalc();
-				$(window).resize(scrollbarWidthCalc);
-			})
-		};
-	};
-	myMenu($('.js-menu'));
+	// // Мобильное меню
+	// function myMenu(menu) {
+	// 	if (menu.length) {
+	// 		menu.each(function () {
+	// 			var $this = $(this),
+	// 					menuBtn = $this.find('#menu-btn'),
+	// 					over = $this.find('#menu-over'),
+	// 					close = $this.find('#menu-close'),
+	// 					body = $('body'),
+	// 					scrollbarWidth;
+	// 			menuBtn.on('click', toggleOpenMenu);
+	// 			over.on('click', menuClose);
+	// 			close.on('click', menuClose);
+	// 			function menuOpen() { // Открывание меню
+	// 				body.addClass('lock').css('padding-right', scrollbarWidth);
+	// 				$this.addClass('open');
+	// 				menuBtn.addClass('is-active');
+	// 			}
+	// 			function menuClose() { // Закрывание меню
+	// 				body.removeClass('lock').css('padding-right', 0);
+	// 				$this.removeClass('open');
+	// 				menuBtn.removeClass('is-active');
+	// 			}
+	// 			function scrollbarWidthCalc() { // Вычисление ширины скролла
+	// 				var documentWidth = parseInt(document.documentElement.clientWidth),
+	// 						windowsWidth = parseInt(window.innerWidth);
+	// 						scrollbarWidth = windowsWidth - documentWidth;
+	// 			}
+	// 			function toggleOpenMenu() { // Открывание/закрывание меню
+	// 				if ($this.hasClass('open')) {
+	// 					menuClose();
+	// 				}else {
+	// 					menuOpen();
+	// 				}
+	// 			}
+	// 			scrollbarWidthCalc();
+	// 			$(window).resize(scrollbarWidthCalc);
+	// 		})
+	// 	};
+	// };
+	// myMenu($('.js-menu'));
 
 	// // Блок с высотой окна браузера
 	// function screenHeight(fullHeight) {
@@ -143,38 +133,38 @@ $(document).ready(function () {
 	// }
 	// tabs($('.js-tabs'));
 
-	// // Аккордеон
-	// function accordion() {
-	// 	if ($('.accordion').length) {
-	// 		$('.accordion').each(function () {
-	// 			var accordion = $(this),
-	// 				trigger = accordion.find('.accordion__trigger'),
-	// 				time = 300;
-	// 			trigger.on('click', function () {
-	// 				var $thisTrigger = $(this),
-	// 					data = $thisTrigger.data('trigger');
-	// 				if (!$thisTrigger.hasClass('active')) {
-	// 					$thisTrigger.addClass('active');
-	// 					accordion.find('#' + data).stop().slideDown(
-	// 						time,
-	// 						function () {
-	// 							$(this).addClass('open')
-	// 						}
-	// 					);
-	// 				} else {
-	// 					$thisTrigger.removeClass('active');
-	// 					accordion.find('#' + data).stop().slideUp(
-	// 						time,
-	// 						function () {
-	// 							$(this).removeClass('open')
-	// 						}
-	// 					);
-	// 				}
-	// 			})
-	// 		})
-	// 	}
-	// }
-	// accordion();
+	// Аккордеон
+	function accordion() {
+		if ($('.accordion').length) {
+			$('.accordion').each(function () {
+				var accordion = $(this),
+					trigger = accordion.find('.accordion__trigger'),
+					time = 300;
+				trigger.on('click', function () {
+					var $thisTrigger = $(this),
+						data = $thisTrigger.data('trigger');
+					if (!$thisTrigger.hasClass('active')) {
+						$thisTrigger.addClass('active');
+						accordion.find('#' + data).stop().slideDown(
+							time,
+							function () {
+								$(this).addClass('open')
+							}
+						);
+					} else {
+						$thisTrigger.removeClass('active');
+						accordion.find('#' + data).stop().slideUp(
+							time,
+							function () {
+								$(this).removeClass('open')
+							}
+						);
+					}
+				})
+			})
+		}
+	}
+	accordion();
 
 	// // Модальное окно
 	// function modal(modal) {
